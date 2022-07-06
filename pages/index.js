dad86c6d87b57195e77d419f58e3fa1df7f97a42
eg-delacruz <netoxas3107@hotmail.com>
2022-07-06 13:22:53 +0200

    Terminado

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ import Test5 from '../components/Test5';
 
 export default function Home() {
   //TODO: set state to 0
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
   return (
     <>
       <Head>
